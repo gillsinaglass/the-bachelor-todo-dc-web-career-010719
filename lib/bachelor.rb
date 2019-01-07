f5e6385e9235b37f0b60ca9e5a6aa1d0ba330e6a
Ryan Gill <rdgill95@gmail.com>
2019-01-07 01:44:20 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,18 +16,17 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  counter = 0
-  data.each do |season, array|
-    array.each do |hash|
-      hash.each do |k,v|
-        if v == hometown
-          counter += 1
+  count = 0
+  data.each do |season, list_items|
+    list_items.each do |cont|
+      cont.each do |key,value|
+        if value == hometown
+          count += 1
         end
       end
     end
   end
-counter
-
+count
 end
 
 def get_occupation(data, hometown)
